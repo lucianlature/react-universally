c8ee78463942f57424c3e2de53b12390f38dd084
Lucian Lature <lucian.lature@gmail.com>
2016-12-09 12:37:37 +0000

    Fixed conflicts.

--- src/shared/components/App/App.js
+++ src/shared/components/App/App.js
@@ -8,7 +8,7 @@ import 'normalize.css/normalize.css';
 import './globals.css';
 import Error404 from './Error404';
 import Header from './Header';
-import htmlPageConfig from '../../../../config/public/htmlPage';
+import { WEBSITE_TITLE, WEBSITE_DESCRIPTION } from '../../constants';
 
 function App() {
   return (
@@ -18,12 +18,34 @@ function App() {
         @see https://github.com/nfl/react-helmet
       */}
       <Helmet
-        htmlAttributes={htmlPageConfig.htmlAttributes}
-        titleTemplate={htmlPageConfig.titleTemplate}
-        defaultTitle={htmlPageConfig.defaultTitle}
-        meta={htmlPageConfig.meta}
-        link={htmlPageConfig.links}
-        script={htmlPageConfig.scripts}
+        htmlAttributes={{ lang: 'en' }}
+        titleTemplate={`${WEBSITE_TITLE} - %s`}
+        defaultTitle={WEBSITE_TITLE}
+        meta={[
+          { name: 'description', content: WEBSITE_DESCRIPTION },
+          // Default content encoding.
+          { name: 'charset', content: 'utf-8' },
+          // @see http://bit.ly/2f8IaqJ
+          { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+          // This is important to signify your application is mobile responsive!
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          // Providing a theme color is good if you are doing a progressive
+          // web application.
+          { name: 'theme-color', content: '#2b2b2b' },
+        ]}
+        link={[
+          // When building a progressive web application you need to supply
+          // a manifest.json as well as a variety of icon types. This can be
+          // tricky. Luckily there is a service to help you with this.
+          // http://realfavicongenerator.net/
+          { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+          { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+          { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+          { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#00a9d9' },
+          // Make sure you update your manifest.json to match your application.
+          { rel: 'manifest', href: '/manifest.json' },
+        ]}
+        script={[]}
       />
 
       <Header />
@@ -39,10 +61,28 @@ function App() {
       />
 
       <Match
+        pattern="/posts"
+        render={routerProps =>
+          <CodeSplit chunkName="posts" modules={{ Posts: require('./Posts') }}>
+            { ({ Posts }) => Posts && <Posts {...routerProps} /> }
+          </CodeSplit>
+        }
+      />
+
+      <Match
         pattern="/about"
         render={routerProps =>
           <CodeSplit chunkName="about" modules={{ About: require('./About') }}>
             { ({ About }) => About && <About {...routerProps} /> }
+          </CodeSplit>
+        }
+      />
+
+      <Match
+        pattern="/foo"
+        render={routerProps =>
+          <CodeSplit chunkName="foo" modules={{ Foo: require('./Foo') }}>
+            { ({ Foo }) => Foo && <Foo {...routerProps} /> }
           </CodeSplit>
         }
       />
